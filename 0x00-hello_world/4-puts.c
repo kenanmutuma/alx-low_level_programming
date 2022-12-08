6931008378c0af5ec1258fac52d6e85b64688f0f
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-08 09:19:40 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main = entry
+ * puts = print function
+ * return = 0
+ */
+int main (void)	
+{
+	puts("\"Programming is like building a multilingual puzzle ");
+	return (0);
+}
