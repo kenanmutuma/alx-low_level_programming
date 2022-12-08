9f753622377b72dcd2ab313f28998e4c26d12fc7
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-08 10:02:14 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+
+
 /**
- * main is Entry 
- * Return: Always 0
+ * main - Entry Point
+ *
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
