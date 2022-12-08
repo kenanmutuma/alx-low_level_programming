9436223a5d0cde766720822b2082217bbab5edcf
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-08 11:45:05 -0800

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
