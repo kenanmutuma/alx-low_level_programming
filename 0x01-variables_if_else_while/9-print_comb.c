bb71b62ec57da346ce4d7b6433128575ebd05cc9
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-09 14:32:43 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,12 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a < 10; a++) 
-	{
-		putchar(a + '0');
-		if (a <9) 
-		{
-		
-			putchar(',');
-		        putchar(' ');
-         	}
+for (a = 0; a < 10; a++;)
+{
+	putchar(a + '0');
+if (a < 9)
+	{putchar(',');
+	putchar(' '); }
 	}
 	putchar('\n');
 	return (0);
