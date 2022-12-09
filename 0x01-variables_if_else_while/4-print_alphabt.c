68c464b0458acf7efe3a5ec5e0a886e23775c043
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-09 11:41:41 -0800

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,15 @@
 
 int main(void)
 {
-	char lc;
-	for (lc = 'a'; lc <= 'z'; lc++;)
-        {
-	 
-	  	putchar(lc);
-        } 
+	char c;
 
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
 	putchar('\n');
 	return (0);
 }
