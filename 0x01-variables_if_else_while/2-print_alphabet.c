bbd62adcc8156b4fcc9aca38eb2483603d737e44
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-09 14:48:57 -0800

    task 6

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,11 @@ int main(void)
 {
 	char lc;
 	for (lc = 'a'; lc <= 'z'; lc++;)
-        {
-	 
+       
+       	{
+	
 	  	putchar(lc);
-        } 
+        }
 
 	putchar('\n');
 	return (0);
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,13 +10,11 @@ int main(void)
 	int c;
 
 	c = 0;
-        while 
-		(c < 10) {
+	while
+	(c < 10) {
 			putchar(c + '0');
 			c++;
 		}
 	putchar('\n');
 	return (0);
 }
-	
-
