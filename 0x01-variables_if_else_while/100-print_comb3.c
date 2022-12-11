815f01358ce4134ca90ec0ef8dc42cf5464b9baf
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-11 12:58:40 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)/* print tens digit*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+		{
+			if (!((ones == tens) || (tens > ones)))/*eliminates replication */
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/*adds comma and spaces*/
+				{
+					putchar(',');
+					putchar(',');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
