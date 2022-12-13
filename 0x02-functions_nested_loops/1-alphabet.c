b5cf95f731d8c3a76fb37e47802eeae0526cf668
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-13 13:57:42 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	-putchar('\n');
+}
