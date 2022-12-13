dc1d82e0f6cfd48a5d22166318de18f6030a1a9b
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-13 12:46:51 -0800

    task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,8 +11,8 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n;)
-void print_times_table(int n);
-void num(int r, int n);
+void 'print_to_98'(int n;)
+void 'print_times_table'(int n);
+void 'num'(int r, int n);
 
 #endif
