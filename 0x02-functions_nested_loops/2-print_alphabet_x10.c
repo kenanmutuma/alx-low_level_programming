5002b31d0229be0f8dec43f87a3ce3ac6bc962a0
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-14 03:24:42 -0800

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char la
+	for (la = 'a'; la <= '10'; la <= 'z'; la++)
+
+	{
+	_putchar(la); }
+
+	_putchar('\n')
+	return (0);
+}
