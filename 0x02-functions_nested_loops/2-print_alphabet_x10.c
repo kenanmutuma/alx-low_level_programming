5d0d97247d06caf13977c9892d25af72ff55ba9f
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-14 04:24:37 -0800

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,10 +2,9 @@
 /**
  * print_alphabet_10 - prints alpha 10 times
  */
-void print_alphabet_x10(void);
-
+void print_alphabet_x10(void)
 {
-	int ten
+	int ten;
 	char la;
 
 	for (ten = 0; ten <= 9; ten++)
