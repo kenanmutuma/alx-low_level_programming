9fef249ef132004b6bafaf00a47c3846af5aa5eb
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-15 12:42:42 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4)
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		if (ch != 2 && ch != 4)
+			_putchar(ch);
+	}
+	_puchar('\n');
+}
