c020260ccef5739171aca3bf7e600c190dcc6694
kenanmutuma <kenanmutuma12@gmail.com>
2022-12-22 13:31:30 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,6 @@
  * Return: pointer to a string
  *
  */
-char *cap_string(char *)
 char *cap_string(char *str)
 {
 	int i = 0;
@@ -28,7 +27,7 @@ char *cap_string(char *str)
 				str[i - 1] == ')' ||
 				str[i - 1] == '{' ||
 				str[i - 1] == '}' ||
-				i == 0;)
+				i == 0)
 			str[i] -= 32;
 		i++;
 	}
