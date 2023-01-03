8fdf370b24d90d478f1a63fa145765be3451f943
kenanmutuma <kenanmutuma12@gmail.com>
2023-01-03 11:10:41 -0800

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - memory set function
+ * @s: string
+ * @b: a character
+ * @n: an integer
+ * Return: a string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+	for (int i = 0; i < n; i++)
+
+	{
+
+		s[i] = b;
+
+	}
+
+	return (s);
+
+}
