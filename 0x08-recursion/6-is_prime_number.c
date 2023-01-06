c797611a46f45ac13fbbb6cded39b58d87136bc8
kenanmutuma <kenanmutuma12@gmail.com>
2023-01-06 12:46:01 -0800

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int is_prime_number - prints prime numbers
+ * is_prime_number - prints prime numbers
  * @n: the number to be printed
  * Return: 1,0
  */
@@ -14,15 +14,16 @@ int is_prime_number(int n)
 	} else if (n <= 3)
 	{
 		return (1);
-       	}
+	}
 	else if (n % 2 == 0 || n % 3 == 0)
 	{
 		return (0);
 	}
-       	else
+	else
 	{
 		int i = 5;
-	       	return (is_prime_number_helper(n, i));
-       	}
+
+		return (is_prime_number_helper(n, i));
+	}
 
 }
