8f3fbcc9af57aeefb618f03ac106ab9a0c0de491
kenanmutuma <kenanmutuma12@gmail.com>
2023-01-11 12:23:39 -0800

    task 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 
-	concat = malloc((len1 + len2) * sizeof(char));
+	concat = malloc((len1 + len2 + 1) * sizeof(char));
 	if (concat == NULL)
 		return (NULL);
 	strcpy(concat, s1);
