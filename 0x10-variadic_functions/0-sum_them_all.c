c4e03a5fd9c8c10c36e8f68c2e4c69d163e97e51
kenanmutuma <kenanmutuma12@gmail.com>
2023-01-22 11:45:43 -0800

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
-	va_end(valist0);
+	va_end(valist);
 
 	return (sum);
 }
